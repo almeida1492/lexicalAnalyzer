84127c05d8b3a5b9cf701fbd68fdbd1565ba330e
Henrique de Almeida <henriquealmeida1492@gmail.com>
2017-05-02 15:18:36 -0300

    Apply changes (t_) to all reserved words

--- isKeyword.h
+++ isKeyword.h
@@ -8,14 +8,38 @@ bool isKeyword(char *lexeme, tokens *theCounter, FILE *file){
 	if (strcmp(lexeme, "program") == 0){
 		theCounter->keywordCount++;
 		fprintf(file, "t_program ");
-	}
-	if (strcmp(lexeme, "program")	== 0	||
-		strcmp(lexeme, "var")		== 0	||
-		strcmp(lexeme, "begin")		== 0	||
-		strcmp(lexeme, "loop") 		== 0	||
-		strcmp(lexeme, "integer") 	== 0	||
-		strcmp(lexeme, "end")		== 0	){
 		return true;
 	}
+
+	if (strcmp(lexeme, "var") == 0){
+		theCounter->keywordCount++;
+		fprintf(file, "t_var ");
+		return true;
+	}
+
+	if (strcmp(lexeme, "begin") == 0){
+		theCounter->keywordCount++;
+		fprintf(file, "t_begin ");
+		return true;
+	}
+
+	if (strcmp(lexeme, "loop") == 0){
+		theCounter->keywordCount++;
+		fprintf(file, "t_loop ");
+		return true;
+	}
+
+	if (strcmp(lexeme, "integer") == 0){
+		theCounter->keywordCount++;
+		fprintf(file, "t_integer ");
+		return true;
+	}
+
+	if (strcmp(lexeme, "end") == 0){
+		theCounter->keywordCount++;
+		fprintf(file, "t_end ");
+		return true;
+	}
+	
 	return false;
 }
